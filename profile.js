880824100f63ffad7fb5a41bb518fc888d1481ca
JadenFicklin <jadendficklin@gmail.com>
2022-01-07 12:01:35 -0700

    5 completed updated neighborhood

--- profile.js
+++ profile.js
@@ -1,4 +1,4 @@
-console.log("hello world");
+// functionality for buttons
 
 const getColor = document.querySelector("#color");
 const getPlace = document.querySelector("#place");
@@ -19,3 +19,28 @@ function handleRitual() {
 getColor.addEventListener("click", handleColor);
 getPlace.addEventListener("click", handlePlace);
 getRitual.addEventListener("click", handleRitual);
+
+//functionality for form I put in
+const getButton = document.querySelector(".profile-button");
+const getYes = document.querySelector("#yes");
+const getYesLabel = document.querySelector(".yes-label");
+const getNo = document.querySelector("#no");
+
+// trying to remove yes so the value of getYes will be no
+function toggleYes() {
+  getYes.remove();
+  getYesLabel.remove();
+}
+
+getNo.addEventListener("click", toggleYes);
+
+function handleButton(event) {
+  event.preventDefault();
+
+  if (getNo.value === "no") {
+    alert("what");
+  } else if (getYes.value === "yes") {
+    alert("good choice");
+  }
+}
+document.querySelector("form").addEventListener("submit", handleButton);
