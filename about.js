3710b3422597fc5053ab5fc57ef96a8b4746c00c
JadenFicklin <jadendficklin@gmail.com>
2022-01-07 09:36:49 -0700

    completed part 2

--- about.js
+++ about.js
@@ -1,14 +1,17 @@
 console.log("hello world");
 
-
-
 function handleSubmit(evt) {
-	evt.preventDefault();
-	
-	console.log('form submit');
+  evt.preventDefault();
+
+  //   console.log("form submit");
+  alert("the form has been submitted successfully");
 }
+function handleImage(evt) {
+  evt.preventDefault();
+  alert("you're cool");
+}
+let form = document.querySelector("form#contact");
 
-
-let form = document.querySelector('form#contact');
-
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("submit", handleSubmit);
+let catImage = document.querySelector("#cat-image");
+catImage.addEventListener("mouseover", handleImage);
